ef7c445bce04967e0a94abff84e59e6c9184ff59
Randi Mohorović <92921082+randimohorovic@users.noreply.github.com>
2022-11-15 09:34:11 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
             <button
               class="navbar-toggler"
               type="button"
-              data-toggle="collapse"
-              data-target="#navbarTogglerDemo01"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarTogglerDemo01"
               aria-controls="navbarTogglerDemo01"
               aria-expanded="false"
               aria-label="Toggle navigation"
